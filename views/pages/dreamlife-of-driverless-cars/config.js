9e403605170efbdafb84e2e9c503c24119684898
vonKristoff <mrjcnicolas@gmail.com>
2017-06-06 14:21:06 +0100

    add example alternative page|route

---
+++ views/pages/dreamlife-of-driverless-cars/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    body: {
+        title: "Dream Life of Driverless Cars",
+        client: "New York Times Magazine",
+        video: "https://player.vimeo.com/video/145248208",
+        label: "",
+        summary: ""
+    },
+    meta: {
+        href: "dreamlife-of-driverless-cars",
+        title: "Dream Life of Driverless Cars | New York Times Magazine",
+        description: "",
+        active: true,
+        tags: ['film', 'artwork']
+    },
+    script: {
+        use: false,
+        src: "/js/dreamlifeofdriverlesscars.js"
+    },
+    images: []
+}
