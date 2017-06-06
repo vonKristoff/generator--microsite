1257bc129aae4bdf53f6917e5442c6265d56c7b3
vonKristoff <mrjcnicolas@gmail.com>
2017-06-06 14:12:20 +0100

    add page boilerplates

---
+++ _raw/directory/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    body: {
+        title: "__id",
+        client: "",
+        label: "",
+        summary: "",
+        video: null
+    },
+    meta: {
+        href: "__ref",
+        title: "__id",
+        description: "",
+        active: true,
+        tags: []
+    },
+    script: {
+        use: false,
+        src: "/js/__id.js"
+    },
+    images: []
+}
